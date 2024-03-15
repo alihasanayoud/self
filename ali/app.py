6cbfb4cbf39a2e1b0880c23246010ae861ead6b8
alihasanayoud <aliayoud.ai.12@gmail.com>
2024-03-15 20:19:47 +0300

    first commit

---
+++ ali/app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_docker():
+    return '<h1> hello world </h1'
+for i in range(10):
+    print(int)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
